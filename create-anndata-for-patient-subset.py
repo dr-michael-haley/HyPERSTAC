cfbb5987166ca7f06213a768c2da3cf87501ffbb
Lucas Farndale <67098917+lucasfarndale@users.noreply.github.com>
2025-09-30 14:18:34 +0100

    Update create-anndata-for-patient-subset.py

--- create-anndata-for-patient-subset.py
+++ create-anndata-for-patient-subset.py
@@ -27,6 +27,52 @@ def get_patient_id(x):
     patient_label = x.split('/')[-1].split('_')[1]
     return patient_label
 
+def create_hdf5_for_subset(save_path, rep_path, ids, num_folds=2, fold=0, scanpy_package=rsc, resolution=1):
+    paths = glob.glob(os.path.join(rep_path,'*'))
+    
+    # Extracts patient IDs to make sure two slides from the same patient are assigned to the same fold
+    case_numbers = []
+    for path in paths:
+        patient_label = int(get_patient_id(path))
+        case_numbers.append(patient_label)
+    case_numbers = sorted(list(set(case_numbers)))
+    
+    train_cases = [case_id for case_id in case_numbers if (case_id%NUM_FOLDS != fold) & (case_id in ids)]
+    valid_cases = [case_id for case_id in case_numbers if (case_id%NUM_FOLDS == fold) & (case_id in ids)]
+        
+    train_paths = [path for path in paths if int(get_patient_id(path)) in train_cases]
+    valid_paths = [path for path in paths if int(get_patient_id(path)) in valid_cases]
+        
+    train_features = np.concatenate([read_features(i) for i in train_paths if os.path.exists(i)], axis=0)
+    valid_features = np.concatenate([read_features(i) for i in valid_paths if os.path.exists(i)], axis=0)
+
+    train_patch_paths = np.concatenate([read_paths(i) for i in train_paths if os.path.exists(i)], axis=0)
+    valid_patch_paths = np.concatenate([read_paths(i) for i in valid_paths if os.path.exists(i)], axis=0)
+    
+    adata = ad.AnnData(train_features)
+    scanpy_package.pp.neighbors(adata)
+    scanpy_package.tl.umap(adata)
+    scanpy_package.tl.leiden(adata, resolution=resolution)
+    cluster_labels_old = np.array(sorted(list(adata.obs['leiden'].unique().astype(int)))).astype(str)
+    cluster_labels_new = np.arange(len(cluster_labels_old))
+    map_dict = {cluster_labels_old[i]: cluster_labels_new[i] for i in range(len(cluster_labels_old))}
+    adata.obs['leiden'] = list(map(lambda x: str(map_dict[x]), adata.obs['leiden']))
+    adata.obs['patch_path'] = train_patch_paths
+    adata_valid = ad.AnnData(valid_features)
+    adata_valid.obs['patch_path'] = valid_patch_paths
+    adata.obs['sample_id'] = adata.obs['patch_path'].astype(str).apply(lambda x: x.split('/')[-2])
+    adata.obs['samples'] = adata.obs['sample_id']
+    adata_valid.obs['sample_id'] = adata_valid.obs['patch_path'].astype(str).apply(lambda x: x.split('/')[-2])
+    adata_valid.obs['samples'] = adata_valid.obs['sample_id']
+    scanpy_package.pp.neighbors(adata_valid)
+    scanpy_package.tl.umap(adata_valid)
+    sc.tl.ingest(adata_valid, adata, obs='leiden', embedding_method='umap')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    adata.write(os.path.join(save_path, "adata-train.hdf5"))
+    adata_valid.write(os.path.join(save_path, "adata-valid.hdf5"))
+    return adata, adata_valid
+
 pd1_df = pd.read_csv("ki67_pdl1_df.csv", index_col=0)
 
 pd1_pos = pd1_df[pd1_df['PDL_positive']==1].samples
